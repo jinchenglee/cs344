ca671cc35a6fc0ebbf80d233c35838d6a9eeab55
jinchenglee <jincheng.li@gmail.com>
2017-06-21 11:10:46 -0700

    Specific changes for my specific home Ubuntu machine. Opencv 2.4.9 is used and the machine has opencv3.1 as default.

--- Problem Sets/Problem Set 1/HW1.cpp
+++ Problem Sets/Problem Set 1/HW1.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 #include "utils.h"
 #include <cuda.h>
